2a9f33bf25aba3d5f1948d005dbce93168d91cd4
lor444 <lor.7@live.it>
2022-12-24 01:52:39 +0100

    first commit

---
+++ main.ts
@@ -0,0 +1,29 @@
+console.log("Hello World!");
+console.log("🏆This is my first Node Project!");
+
+let myVar = 3;
+myVar += 10; // 13;
+
+console.log('myVar = ', myVar);
+
+interface MyInterface {
+  myNumber: number;
+  myString: string;
+}
+
+let myInterface: MyInterface = {
+  myNumber: 10,
+  myString: 'Hello world',
+};
+
+console.log('myNumber = ', myInterface.myNumber);
+console.log('myString = ', myInterface.myString);
+
+
+
+
+
+
+
+
+
